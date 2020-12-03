25194bf9e8026ccd07431be8493e9dd330fc0b43
Tony Reurink <loch.hollows@gmail.com>
2020-12-03 17:13:25 -0500

    Switched to a blocklist, added URL validation

--- main.js
+++ main.js
@@ -3,19 +3,25 @@ const saveLinks = require("./db/insert.js");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
-const channelsAllowed = ["general"];
+const BLOCKED =
+    process.env.CHANNELS == "" ? process.env.CHANNELS.split(",") : "";
+const { isURL } = require("validator");
 
 bot.login(TOKEN);
 
 // On message, check for links and trigger save func
 bot.on("message", async (message) => {
     let channel = await message.channel.fetch();
-    if (channelsAllowed.includes(channel.name)) {
+    console.log("Current channel: " + channel.name);
+    if (!BLOCKED.includes(channel.name)) {
+        console.log("Channel acceptable");
         let links = message.content.match(/\bhttps?:\/\/\S+/gi) || null;
-        if (links) {
+        let validatedLinks = links ? links.filter((link) => isURL(link)) : null;
+        if (validatedLinks.length > 0) {
+            console.log("Links valid, saving to database.");
             let linksData = {
                 user: message.author.username,
-                links: links,
+                links: validatedLinks,
                 time: Date.now(),
                 channel: channel.name,
             };
