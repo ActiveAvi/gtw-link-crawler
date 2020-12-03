719d9efbefb4ce9cd4abd3befb412983ac2014b8
Tony Reurink <loch.hollows@gmail.com>
2020-12-03 11:11:27 -0500

    Removed needless async / await

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const TOKEN = process.env.TOKEN;
 bot.login(TOKEN);
 
 // On message, check for links and trigger save func
-bot.on("message", async (message) => {
+bot.on("message", (message) => {
     let links = message.content.match(/\bhttps?:\/\/\S+/gi) || null;
     if (links) {
         let linksToSave = {
@@ -16,6 +16,6 @@ bot.on("message", async (message) => {
             links: links,
             time: Date.now(),
         };
-        await saveLinks(linksToSave);
+        saveLinks(linksToSave);
     }
 });
