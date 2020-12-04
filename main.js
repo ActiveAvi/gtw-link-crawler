c2edc0fb0ee741bd41f3f60fa1bb96ffbdf2814b
Tony Reurink <loch.hollows@gmail.com>
2020-12-03 21:02:28 -0500

    + login success and fail msgs. url valid repriorit

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 require("dotenv").config();
 const saveLinks = require("./db/insert.js");
+const LinksList = require("./models/LinksList.js");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 const BLOCKED =
     process.env.CHANNELS == "" ? process.env.CHANNELS.split(",") : "";
 const { isURL } = require("validator");
+const LinkList = require("./models/LinksList.js");
 
-bot.login(TOKEN);
+bot.login(TOKEN)
+    .then(() => console.log("Login successful"))
+    .catch((err) => console.error(err));
 
 // On message, check for links and trigger save func
 bot.on("message", async (message) => {
+    let user = message.author.username;
     let channel = await message.channel.fetch();
     if (!BLOCKED.includes(channel.name)) {
-        let links = message.content.match(/\bhttps?:\/\/\S+/gi) || null;
-        let validatedLinks = links.filter((link) => isURL(link));
-        if (!Array.isArray(validatedLinks) || validatedLinks.length > 0) {
-            let linksData = {
-                user: message.author.username,
-                links: validatedLinks,
-                time: Date.now(),
-                channel: channel.name,
-            };
-            saveLinks(linksData);
+        let links = message.content.match(/\bhttps?:\/\/\S+/gi) || [];
+        links = links.filter(link => isURL(link));
+        if(links) {
+            let linkList = new LinkList({
+                user: user,
+                channel: channel,
+                timePosted: Date.now(),
+                links: links,
+            });
+            saveLinks(linkList)
         }
     }
 });
