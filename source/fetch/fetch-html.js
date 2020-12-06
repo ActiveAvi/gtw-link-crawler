32c7f6eda81dcd930ce8455ffdc5b94b828221cf
Tony Reurink <loch.hollows@gmail.com>
2020-12-06 18:07:24 -0500

    Used proxy variable instead of this reference.

--- source/fetch/fetch-html.js
+++ source/fetch/fetch-html.js
@@ -4,7 +4,7 @@ let fetchHtml = async function () {
     let link = this.link;
     console.log('Fetching: ' + link);
     try {
-        let res = await fetch(this.link);
+        let res = await fetch(link);
         res = await res.text();
         return res;
     } catch (err) {
