f0661b8f26072a95ce63b7bcd127f39c1a58ed01
Tony Reurink <loch.hollows@gmail.com>
2020-12-06 16:24:18 -0500

    Refactoring

--- db/connect.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-const DBURL = process.env.DBURL;
-const nano = require("nano")(DBURL);
-const db = nano.db.use("links");
-
-module.exports = db;
--- sorter/sort.js
+++
@@ -1,6 +0,0 @@
-function sort(link) {
-    // console.log(link)
-    // console.log(module.exports)
-}
-
-module.exports = sort;
