2f3064ae38aa2c9fcaab494862579d1a1c7d202e
Tony Reurink <loch.hollows@gmail.com>
2020-12-07 13:55:23 -0500

    Added join path method to fix undefined envvar bug

--- source/db/connect.js
+++ source/db/connect.js
@@ -1,4 +1,5 @@
-require("dotenv").config({ path: "../../.env" });
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, '.env') });
 const DBURL = process.env.DBURL;
 const nano = require("nano")(DBURL);
 const db = nano.db.use("links");
